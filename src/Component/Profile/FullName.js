634ef808c3821c132383f52f5a315efdfe5ecde9
AzizBousnina <azizbousnina2000@gmail.com>
2023-01-31 01:47:58 +0100

    first commit

---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FullName.css'
+ 
+ const FullName = () => {
+return (
+    <div>
+
+<h2>Full Name : Aziz Bousnina</h2>
+    </div>
+)
+ }
+export default FullName
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,13 @@
+ import React from 'react'
+ import './ProfilePhoto.css'
+ const ProfilePhoto = () => {
+return (
+    <div> 
+    
+    <img src='https://scontent.ftun8-1.fna.fbcdn.net/v/t39.30808-6/307485286_5788316844548052_3751687031008093998_n.jpg?stp=dst-jpg_s960x960&_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=WADBVPY78noAX9w1REk&_nc_ht=scontent.ftun8-1.fna&oh=00_AfBkzSUdWRJ1bxY48TdCvUfAW6UNK-XcEVo5gAm5RyXAHw&oe=63DD2479' alt='pic' />
+    
+    </div>
+)
+ }
+
+ export default ProfilePhoto
